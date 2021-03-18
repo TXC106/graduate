5322dfacd70c52e952aeaa6690cd483423022754
Jane <Jane694194716@gmail.com>
2021-03-18 14:36:10 +0800

    从加fake-useragent往后

--- jobsearch.py
+++ jobsearch.py
@@ -5,6 +5,7 @@
 # @Project : spider
 import csv
 import random
+from fake_useragent import UserAgent
 
 import requests
 import re
@@ -18,14 +19,16 @@ s.keep_alive = False
 # 增加重试连接次数
 requests.adapters.DEFAULT_RETRIES = 5
 
-proxies_list = ['113.194.30.162:9999', '122.224.65.197:3128', '110.243.19.138:9999',
-                '49.67.55.216:8118', '175.43.56.13:9999']
+# proxies_list = ['60.191.11.241:3128', '122.224.65.197:3128', '110.243.19.138:9999',
+#                 '49.67.55.216:8118', '175.43.56.13:9999']
+# proxies_list = ['117.90.240.75:8118','60.191.11.241:3128']
+proxies_list = ['49.86.56.40:9999']
 proxy_num = 0
 
 
 def getProxy(num=0):
     proxies = {
-        'http': '118.24.88.66:1080',
+        'http': '106.14.198.6:8080',
         'https': proxies_list[num]
     }
     return proxies
@@ -90,36 +93,128 @@ def getJobInfo():
     job_info_sum_dict = {}   # 总清单
     while page_num <= sum_num:
         url_Announce = 'https://search.51job.com/list/090200,000000,0000,00,9,99,%25E8%25AE%25A1%25E7%25AE%2597%25E6%259C%25BA,2,{}.html'.format(page_num)
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
-                          ' Chrome/77.0.3865.120 Safari/537.36'
-        }
-        headers["X-Forwarded-For"] = "%s.%s.%s.%s" % (
-            random.randrange(1, 200, 20), random.randrange(1, 200, 20), random.randrange(1, 200, 20),
-            random.randrange(1, 200, 20))
+        # headers = {
+        #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
+        #                   ' Chrome/77.0.3865.120 Safari/537.36'
+        # }
+        # headers["X-Forwarded-For"] = "%s.%s.%s.%s" % (
+        #     random.randrange(1, 200, 20), random.randrange(1, 200, 20), random.randrange(1, 200, 20),
+        #     random.randrange(1, 200, 20))
+        # headers = {
+        #     'User-Agent': UserAgent(verify_ssl=False).random
+        # }
+
+        # for page_again in range(3):
+        #     try:
+        #         # http连接数量控制
+        #         s.headers = UserAgent(verify_ssl=False).random
+        #         print("0")
+        #         print(s.headers)
+        #         # 换代理
+        #         # proxy_num = proxy_num
+        #         # s.proxies = getProxy(proxy_num % 2)
+        #         # s.proxies = {}
+        #         # print("the current proxy ip0 is")
+        #         # print(s.proxies)
+        #         resp = s.get(url_Announce)
+        #         # resp = requests.get(url_Announce, headers=headers)
+        #         # print(resp.status_code)
+        #     except:
+        #         for page_except_again in range(3):
+        #             try:
+        #                 s.headers = UserAgent(verify_ssl=False).random
+        #                 print("1")
+        #                 print(s.headers)
+        #                 print("cannot get access to it0")
+        #                 # time.sleep(5)
+        #                 # proxy_num = proxy_num+1
+        #                 # if proxy_num%2 == 0:
+        #                 #     s.proxies = {}
+        #                 # else:
+        #                 #     s.proxies = getProxy(proxy_num%2)
+        #                 s.proxies = getProxy()
+        #                 print("the current proxy ip0 is")
+        #                 print(s.proxies)
+        #                 resp = s.get(url_Announce)
+        #             except:
+        #                 time.sleep(3)
+        #             else:
+        #                 break
+        #
+        #         # 换代理
+        #         # for p in range(0,5):
+        #         #     try:
+        #         #         proxy_num = proxy_num+1
+        #         #         s.proxies = getProxy(proxy_num%5)
+        #         #         print("the current proxy ip0 is")
+        #         #         print(s.proxies)
+        #         #         resp = s.get(url_Announce)
+        #         #         break
+        #         #     except:
+        #         #         continue
+        #         # # print(resp.status_code)
+        #     else:
+        #         break
 
         try:
             # http连接数量控制
-            s.headers = headers
+            s.headers = UserAgent(verify_ssl=False).random
+            print("0")
+            print(s.headers)
             # 换代理
-            proxy_num = proxy_num
-            s.proxies = getProxy(proxy_num % 5)
-            print("the current proxy ip is")
-            print(s.proxies)
+            # proxy_num = proxy_num
+            # s.proxies = getProxy(proxy_num % 2)
+            # s.proxies = {}
+            # print("the current proxy ip0 is")
+            # print(s.proxies)
             resp = s.get(url_Announce)
             # resp = requests.get(url_Announce, headers=headers)
             # print(resp.status_code)
         except:
-            s.headers = headers
-            print("cannot get access to it")
-            time.sleep(5)
-            # 换代理
-            proxy_num = proxy_num+1
-            s.proxies = getProxy(proxy_num%5)
-            print("the current proxy ip is")
-            print(s.proxies)
-            resp = s.get(url_Announce)
-            # print(resp.status_code)
+            for page_except_again in range(5):
+                try:
+                    s.headers = UserAgent(verify_ssl=False).random
+                    print("1")
+                    print(s.headers)
+                    print("cannot get access to it0")
+                    # time.sleep(5)
+                    # proxy_num = proxy_num+1
+                    # if proxy_num%2 == 0:
+                    #     s.proxies = {}
+                    # else:
+                    #     s.proxies = getProxy(proxy_num%2)
+
+                    if page_except_again == 3:
+                        time.sleep(10)
+                        s.proxies = {}
+                        try:
+                            resp = s.get(url_Announce)
+                        except:
+                            continue
+                        else:
+                            break
+
+                    s.proxies = getProxy()
+                    print("the current proxy ip0 is")
+                    print(s.proxies)
+                    resp = s.get(url_Announce)
+                except:
+                    time.sleep(3)
+                else:
+                    break
+
+                # 换代理
+                # for p in range(0,5):
+                #     try:
+                #         proxy_num = proxy_num+1
+                #         s.proxies = getProxy(proxy_num%5)
+                #         print("the current proxy ip0 is")
+                #         print(s.proxies)
+                #         resp = s.get(url_Announce)
+                #         break
+                #     except:
+                #         continue
+                # # print(resp.status_code)
 
         # resp.encoding = 'gbk'
         resp.encoding = resp.apparent_encoding
@@ -133,9 +228,9 @@ def getJobInfo():
         result_full = result_full.split('},{')
 
         # 每条结果对应详情
-        i = 0
+        each_inpage = 0
         end_add = '}'
-        while i < len(result_full):
+        while each_inpage < len(result_full):
         # while result_full[i] != None:
         #     print('==================================')
             # print(result_full[i] + end_add) # 首页展示信息
@@ -143,7 +238,7 @@ def getJobInfo():
             # print(i)
             job_info_dict = {}
             job_info_list = []
-            result_full_now = result_full[i]
+            result_full_now = result_full[each_inpage]
             # 获得详情页面链接
             result_urls = re.findall('"job_href":"(.*?)","job_name"', result_full_now, re.I)[0]
             result_urls_text = re.sub(r'\\', '', result_urls)
@@ -162,6 +257,7 @@ def getJobInfo():
                 # print('result_providesalary_text:'+result_providesalary_text)
                 # print('result_attribute_text:'+result_attribute_text)
                 result_time = re.findall('"updatedate":"(.*?)",', result_full_now, re.I)[0]
+                jobwelf_list = re.findall('"jobwelf_list":(.*?),"attribute_text"', result_full_now, re.I)[0]
                 # print(result_time)
 
                 # 判断详情列表是否有学历
@@ -181,24 +277,81 @@ def getJobInfo():
                     'area': result_attribute_text[1:-1].split(',')[0][1:-1],
                     'experence': experence,
                     'education': education,
-                    'result_time': result_time
+                    'result_time': result_time,
+                    'jobwelf_list': jobwelf_list
                 }
                 # print(result_attribute_text)
                 # print(len(result_attribute_text[1:-1].split(',')))
                 # print(result_attribute_text[1:-1].split(',')[0][1:-1])
                 job_info_dict.update(main_temp_dict)
             except:
-                i = i + 1
+                each_inpage = each_inpage + 1
                 continue
                 # print(i)
 
             # 获得详情界面信息
             # print('==================detail========================')
+            for detail_again in range(3):
+                try:
+                    # http连接数量控制
+                    # s.headers = headers
+                    s.headers = UserAgent(verify_ssl=False).random
+                    print("2")
+                    print(s.headers)
+                    # 换代理
+                    # proxy_num = proxy_num
+                    # s.proxies = getProxy(proxy_num % 2)
+                    # print("the current proxy ip is")
+                    # print(s.proxies)
 
-            # http连接数量控制
-            s.headers = headers
-            resp_detail = s.get(result_urls_text)
-            #  = requests.get(result_urls_text, headers=headers)
+                    resp_detail = s.get(result_urls_text)
+                    #  = requests.get(result_urls_text, headers=headers)
+                except:
+                    for detail_except_again in range(3):
+                        try:
+                            s.headers = headers
+                            print("cannot get access to it")
+                            # time.sleep(3)
+                            # proxy_num = proxy_num + 1
+                            s.proxies = getProxy()
+                            s.headers = UserAgent(verify_ssl=False).random
+                            print("3")
+                            print(s.headers)
+                            print("the current proxy ip is")
+                            print(s.proxies)
+                            # s.headers = headers
+                            # print("cannot get access to it")
+                            # time.sleep(5)
+                            # # 换代理
+                            # for p in range(0, 5):
+                            #     try:
+                            #         proxy_num = proxy_num + 1
+                            #         s.proxies = getProxy(proxy_num % 5)
+                            #         print("the current proxy ip is")
+                            #         print(s.proxies)
+                            #         resp = s.get(url_Announce)
+                            #         break
+                            #     except:
+                            #         continue
+                            if detail_except_again == 2 & detail_again == 1:
+                                time.sleep(10)
+                                s.proxies = {}
+                                try:
+                                    resp = s.get(url_Announce)
+                                except:
+                                    continue
+                                else:
+                                    break
+                            resp_detail = s.get(result_urls_text)
+                        except:
+                            time.sleep(3)
+                        else:
+                            break
+                    break
+                else:
+                    break
+
+
 
             # resp_detail.encoding = 'gbk'
             resp_detail.encoding = resp_detail.apparent_encoding
@@ -258,7 +411,7 @@ def getJobInfo():
                 job_detail = 'noinfo'
 
                 # print(t)
-            i = i + 1
+            each_inpage = each_inpage + 1
             # print(i)
 
             # # 写入txt
@@ -272,12 +425,13 @@ def getJobInfo():
             with open('./jobdata.csv', 'a+',newline='',encoding='utf-8-sig') as csv_file:
                 csv.writer(csv_file).writerow([result_name,result_providesalary_text,job_info_dict['area'],
                                               experence,education,belongs,
-                                               keywords,category_line,category,job_detail,result_time])
+                                               keywords,category_line,category,job_detail,jobwelf_list,result_time])
 
 
             job_info_list.append(job_info_dict)
             # print('=============================')
-            print(i)
+            print(each_inpage)
+            print("done")
             time.sleep(0.1)
             # resp_detail.close()
 
@@ -310,15 +464,19 @@ def getJobInfo():
     # }
     # return announce_sum
     job_info_sum_dict.update({'jobinfo': job_info_list})
-    print(job_info_sum_dict)
+
+    # write json to file
+    with open('./jobjson.txt', 'a+', encoding='utf-8') as json_file:
+        json_file.write(str(job_info_sum_dict) + '\n')
+
     return job_info_sum_dict
 
 
 if __name__ == "__main__":
     # 写入csv首行
-    with open('./jobdata.csv', 'a+', newline='', encoding='utf-8-sig') as csv_file:
-        writer = csv.writer(csv_file)
+    with open('./jobdata.csv', 'a+', newline='', encoding='utf-8-sig') as csv_file_title:
+        writer = csv.writer(csv_file_title)
         writer.writerow(['result_name','result_providesalary_text','area','experence',
-                         'education','belongs','keywords','category_line','category','job_detail','result_time'])
+                         'education','belongs','keywords','category_line','category','job_detail','jobwelf_list','result_time'])
 
     getJobInfo()
